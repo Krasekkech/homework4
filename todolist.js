0a3daf65b4587d25c678a2cbb13fc3fc2c72e53a
Krasekkech <kaml-03@mail.ru>
2023-11-07 18:27:02 +0300

    добавил комментарии в js коде

--- todolist.js
+++ todolist.js
@@ -1,9 +1,13 @@
+// селекторы
 const todoInput = document.querySelector('.todo_input');
 const todoButton = document.querySelector('.todo_button');
 const todoList = document.querySelector('.todo_list-container')
+
+// создаем "слушателей" для наших действий
 todoButton.addEventListener("click", createTodo)
 todoList.addEventListener("click", CRUD)
 
+// создание контейнера для наших задач
 function createTodo(event){
     event.preventDefault();
     const newDiv = document.createElement('div');
@@ -11,12 +15,12 @@ function createTodo(event){
     const newList = document.createElement('li');
     newList.innerText = todoInput.value;
 
-
+    // кнопка выполненной задачи
     const doneTaskButton = document.createElement('button');
     doneTaskButton.classList.add('todo_list-donetask');
     newDiv.appendChild(doneTaskButton);
 
-
+    // сама задача
     newList.classList.add('todo_list');
     newDiv.appendChild(newList);
     if(todoInput.value === ""){
@@ -25,12 +29,14 @@ function createTodo(event){
     todoList.appendChild(newDiv);
     todoInput.value = ""
 
+    // кнопка удаления задачи
     const deleteTaskButton = document.createElement('button');
     deleteTaskButton.classList.add('todo_list-deletetask');
     deleteTaskButton.innerHTML = 'x';
     newDiv.appendChild(deleteTaskButton);
 }
 
+// подключение действий для кнопок
 function CRUD(e){
     const item = e.target;
     if (item.classList[0] === "todo_list-donetask"){
