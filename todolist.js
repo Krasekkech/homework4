73ba72cd24357f74b21b65cdb92267afb32af093
Krasekkech <kaml-03@mail.ru>
2023-11-10 19:48:01 +0300

    доработал localhost

--- todolist.js
+++ todolist.js
@@ -1,4 +1,5 @@
 window.addEventListener("DOMContentLoaded", loadtask)
+
 // селекторы
 const todoInput = document.getElementById('item');
 const todoButton = document.querySelector('.todo_button');
@@ -7,8 +8,6 @@ const todoList = document.querySelector('.todo_list-container');
 // создаем "слушателей" для наших действий
 todoButton.addEventListener("click", createTodo)
 todoList.addEventListener("click", CRUD)
-
-
 
 // создание контейнера для наших задач
 function createTodo(event){
@@ -30,103 +29,70 @@ function createTodo(event){
         return null;
     }
     todoList.appendChild(newDiv);
-    // todoInput.value = ""
 
     // кнопка удаления задачи
     const deleteTaskButton = document.createElement('button');
     deleteTaskButton.classList.add('todo_list-deletetask');
-    deleteTaskButton.innerHTML = 'x';
     newDiv.appendChild(deleteTaskButton);
 
     saveTaskToLocalStorage(todoInput.value);
     todoInput.value = ""
 }
 
+
 // подключение действий для кнопок
 function CRUD(e){
     const item = e.target;
+    const todo = item.parentElement;
+
     if (item.classList[0] === "todo_list-donetask"){
-        const todo = item.parentElement;
         todo.classList.toggle("doneitem");
     }
+
     if (item.classList[0] === "todo_list-deletetask"){
-        const todo = item.parentElement;
-        localStorage.removeItem(todo.value);
+        todo.classList.toggle("fall")
+        const a = document.getElementsByClassName('fall')[0].innerText;
+        let f = tasks.indexOf(a)
+        tasks.splice(f, 1);
+        localStorage.setItem('item', JSON.stringify(tasks));
         todo.remove();
     }
 }
 
+
+
 let tasks = [];
+
 
 function saveTaskToLocalStorage(task) {
     tasks.push(task);
     localStorage.setItem('item', JSON.stringify(tasks));
-
 }
+
 function loadtask(event) {
-
     event.preventDefault();
-    const todoList = document.querySelector('.todo_list-container')
     const data = JSON.parse(localStorage.getItem('item'));
-
     data.forEach(item => {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('todo');
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('todo');
 
-        // кнопка выполненной задачи
-        const doneTaskButton = document.createElement('button');
-        doneTaskButton.classList.add('todo_list-donetask');
-        newDiv.appendChild(doneTaskButton);
+            // кнопка выполненной задачи
+            const doneTaskButton = document.createElement('button');
+            doneTaskButton.classList.add('todo_list-donetask');
+            newDiv.appendChild(doneTaskButton);
 
-        // сама задача
-        const newList = document.createElement('li');
-        newList.innerText = item;
-        newList.classList.add('todo_list');
-        newDiv.appendChild(newList);
-        todoList.appendChild(newDiv);
-        // todoInput.value = ""
+            // сама задача
+            const newList = document.createElement('li');
+            newList.innerText = item;
+            newList.classList.add('todo_list');
+            newDiv.appendChild(newList);
+            todoList.appendChild(newDiv);
 
-        // кнопка удаления задачи
-        const deleteTaskButton = document.createElement('button');
-        deleteTaskButton.classList.add('todo_list-deletetask');
-        deleteTaskButton.innerHTML = 'x';
-        newDiv.appendChild(deleteTaskButton);
-        saveTaskToLocalStorage(item)
-    });
+            // кнопка удаления задачи
+            const deleteTaskButton = document.createElement('button');
+            deleteTaskButton.classList.add('todo_list-deletetask');
+            // deleteTaskButton.innerHTML = 'x';
+            newDiv.appendChild(deleteTaskButton);
+            saveTaskToLocalStorage(item)
+        });
 }
-// window.onload = loadTasks(tasks);
-//
-// function loadTasks(task){
-//     for (let i = 0; i <= task.length-1; i++){
-//         const newDiv = document.createElement('div');
-//         newDiv.classList.add('todo');
-//         const newList = document.createElement('li');
-//         newList.innerText = localStorage.getItem(i);
-//         newList.classList.add('todo_list');
-//         newDiv.appendChild(newList);
-//         todoList.appendChild(newDiv);
-//     }
-// }
-
-// document.addEventListener('DOMContentLoaded', getTasks);
-
-// const tasks = getTasks();
-// tasks.forEach(function(task) {
-//     // Создание DOM элементов для задач из localStorage и их отображение на странице
-//     const newDiv = document.createElement('div');
-//     newDiv.classList.add('todo');
-//     const newList = document.createElement('li');
-//     newList.innerText = task;
-//
-//     // ... (добавление остальных элементов и их размещение в новом div)
-//
-//     todoList.appendChild(newDiv);
-// });
-
-
-
-// function getTasks() {
-//     tasks = localStorage.getItem(0);
-//     return tasks;
-//
-// }
